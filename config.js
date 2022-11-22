3d8b86a1613b1898c45a5b0fdbda75355896c80b
ditya-astawa <115558329+ditya-astawa@users.noreply.github.com>
2022-11-22 17:53:34 +0800

    Update config.js

--- config.js
+++ config.js
@@ -2,9 +2,9 @@ let fs = require('fs')
 let chalk = require('chalk')
 
 owner = [
-  ['62895330379186'],
-  ['62895330379186'],
-  ['628989513496', 'PEMILIK BOT', true]
+  ['6285237596750'],
+  ['6285237596750'],
+  ['6285237596750', 'PEMILIK BOT', true]
   // [number, dia creator/owner?, dia developer?]
 ] // Put your number here
 mods = [] // Want some help?
@@ -58,7 +58,7 @@ fs.watchFile(file_exif, () => {
 // Sticker WM
 packname = sticker_name
 author = sticker_author
-wm = '© Aine'
+wm = '© Ditya'
 
 Intervalmsg = 1800 //detik
 
